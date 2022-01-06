ae4002660c81523fb97e19f75941ad2b36004fe0
Chaz Simons <chaz.simons@gmail.com>
2022-01-06 08:43:18 -0800

    Incorporates user model into Omniauth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
     nickname = auth_hash['extra']['access_token'].params['screen_name']
     access_token = auth_hash['credentials']['token']
 
-    user = User.find_or_create_by(twitter_id)
+    user = User.find_or_create_by(uid: twitter_id)
     user.update(access_token: access_token, nickname: nickname)
-    
+
     session[:user_id] = user.id
 
     redirect_to '/dashboard'
