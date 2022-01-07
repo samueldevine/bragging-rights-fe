9225b914f63cdc81d1be9745b93c0209249456d4
Chaz Simons <chaz.simons@gmail.com>
2022-01-07 10:40:45 -0800

    Adds styling and test for welcome page

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'User Dashboard' do
   before :each do
-    @user = User.create!(uid: 123456, nickname: 'chazsimons', access_token: '123')
+    @user = User.create!(uid: "119651388", nickname: 'chazsimons', access_token: '119651388-u4vxcNWjAUXyzUcapo1SfIqExeFDCKEf6zG8PWTB')
   end
+
   it 'displays the users twitter name' do
     visit '/dashboard'
 
--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Welcome Page' do
-  it 'has a way to button in with twitter' do
+  it 'has an option to sign in' do
     visit '/'
-    click_on "Sign in with Twitter"
-    expect(current_path).to be('https://api.twitter.com/oauth/authenticate?oauth_token=nOBzCgAAAAABXyq8AAABfjITlMY')
+    within("center#welcome") do
+      expect(page).to have_content("Bragging Rights")
+      expect(page).to have_content("Sign in to get your brag on:")
+    end
   end
 end
