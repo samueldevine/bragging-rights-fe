fa81a1bb33a91776a9811ac1c20d4184008fc4b4
Chaz Simons <chaz.simons@gmail.com>
2022-01-06 19:24:54 -0800

    Starts styling for user dashboard and landing page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Welcome Page' do
   it 'has a way to button in with twitter' do
     visit '/'
-    click_link "/auth/twitter"
+    click_on "Sign in with Twitter"
     expect(current_path).to be('https://api.twitter.com/oauth/authenticate?oauth_token=nOBzCgAAAAABXyq8AAABfjITlMY')
   end
 end
