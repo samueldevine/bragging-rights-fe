b1daa8b5a34f7c797e5faadc16bfc3935bdc4a5b
Christian Valesares <cvalesares@gmail.com>
2022-01-10 18:27:13 -0500

    add half-baked feature testing for question links

--- spec/features/games/new_spec.rb
+++ spec/features/games/new_spec.rb
@@ -14,11 +14,23 @@ RSpec.describe "New Game" do
   end
 
   it 'can redirect to new question' do
+    #still need sad path testing for incorrect answers
     visit new_game_path
 
-    click_link "Bruce Wayne"
-    expect(current_path).to eq('/quiz')
-
+    find('.answer', match: :first).click
+    save_and_open_page
     expect(page).to have_content("Question 2")
+
+    find('.answer', match: :first).click
+    expect(page).to have_content("Question 3")
+
+    find('.answer', match: :first).click
+    expect(page).to have_content("Question 4")
+
+    find('.answer', match: :first).click
+    expect(page).to have_content("Question 5")
+
+    find('.answer', match: :first).click
+    expect(current_path).to eq(final_score)
   end
 end
