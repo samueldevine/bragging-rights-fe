cfa173b12bd75547680cbe7527bf7b3521ae68d7
Sam Devine <samuel.will.devine@gmail.com>
2022-01-09 19:37:20 -0700

    Game and Question Poros - update poros and test them
    Co-authored-by: Matthew Kimball <mekimba6@gmail.com>

--- app/poros/game.rb
+++ app/poros/game.rb
@@ -1,23 +1,38 @@
 class Game
-  attr_reader :uid,
-              :score,
-              :city,
-              :state,
-              :country,
-              :correct,
-              :time
+  attr_reader   :user_id,
+                :city,
+                :state,
+                :country
+
+  attr_accessor :score,
+                :questions,
+                :correct,
+                :time
 
   def initialize(data)
-    @uid     = data[:uid]
-    @score   = data[:score]
-    @city    = data[:city]
-    @state   = data[:state]
-    @country = data[:country]
-    @correct = 0
-    @time    = 0
+    @user_id   = data[:user_id]
+    @score     = 0
+    @city      = data[:city]
+    @state     = data[:state]
+    @country   = data[:country]
+    @questions = create_questions(data[:questions]) if data[:questions]
+    @q_count   = count_questions
+    @correct   = 0
+    @time      = 0
   end
 
   def avg_time
-    @time / 5
+    @time / @q_count
+  end
+
+  def create_questions(question_data)
+    question_data.map do |q|
+      Question.new(q)
+    end
+  end
+
+  # call this method on initialization because the game will delete questions as it's played
+  def count_questions
+    @questions.length
   end
 end
--- app/poros/question.rb
+++ app/poros/question.rb
@@ -1,4 +1,17 @@
 class Question
+  attr_reader :id,
+              :question,
+              :correct_answer,
+              :answers
+
   def initialize(data)
+    @id             = data[:id]
+    @question       = data[:question]
+    @correct_answer = data[:correct_answer]
+    @answers        = data[:answers]
+  end
+
+  def correct?(answer)
+    answer == @correct_answer
   end
 end
