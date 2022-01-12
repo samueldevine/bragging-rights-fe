8122958aaef890160155591b4e4185522d104f2c
Christian Valesares <cvalesares@gmail.com>
2022-01-12 14:26:11 -0500

    add finished score by location endpoint and spec

--- spec/facades/scores_facade_spec.rb
+++ spec/facades/scores_facade_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
    describe 'Top Scores' do
      it "displays top scores by query param" do
        score = ScoresFacade.top_scores_by_location("city", "Denver")
-       # user = ScoresFacade.top_scores(69)
+
        expect(score.first).to be_a(Score)
      end
    end
--- spec/poros/score_spec.rb
+++ spec/poros/score_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
  RSpec.describe Score do
-   it "" do
+   it "exists" do
+     score = Score.new(score: 100,
+                user_id: 1,
+                game_time: 1.5,
+                city: "Denver",
+                state: "CO",
+                country: "USA")
 
+    expect(score).to be_an_instance_of(Score)
    end
  end
